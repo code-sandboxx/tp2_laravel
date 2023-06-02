85d53eefd24e326647b7af354c48e8ecb1e057d9
Olena Khliupina <234inbox@gmail.com>
2023-06-02 00:01:48 -0400

    Création de modèles 'Etuidant' et 'Ville', migrations et factories associées

---
+++ database/factories/EtudiantFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Etudiant;
+use App\Models\Ville;
+use Carbon\Carbon;
+
+class EtudiantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $minBirthdate = Carbon::now()->subYears(17)->format('Y-m-d');      
+
+        return [
+            'nom'=>$this->faker->name,
+            'adresse'=>$this->faker->address,
+            'phone'=>$this->faker->phoneNumber,
+            'email'=>$this->faker->unique()->email,
+            'date_de_naissance'=>$this->faker->dateTimeBetween($minBirthdate, 'now')->format('Y-m-d'),
+            'ville_id'=>Ville::factory()
+        ];
+    }
+}

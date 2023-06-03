7fc9298ce3f0b017b1e7c199c485f3ba4188db06
Olena Khliupina <234inbox@gmail.com>
2023-06-03 18:35:34 -0400

    Création du ficher style.css custom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    
+
 </head>
-<body class="bg-light">
+<body class="background vh-100">
+
+    <nav class="navbar primary-bg-color-green navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="navbar-nav flex-grow-1">
+                    <a href="/" class="nav-link fw-bold pl-3">Accueil</a>
+                    <a href="" class="nav-link fw-bold pl-3">Étudiants</a>
+            </div>
+        </div> 
+    </nav>   
+
     @yield('content')
 </body>
+<footer class="footer b-0 py-3 primary-bg-color-green mt-auto fixed-bottom">
+
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
 @section('titleHeader', config('app.name'))
 @section('content')
         <div class="row mt-5 pt-5 justify-content-center">
-            <div class="col-10 text-center">
+            <div class="col-10 text-center flex-grow-1">
                 <p>
-                    <h2 class="text-secondary mt-5 pt-5">Consulter la liste des étudiants du Collège Maisonneuve</h2>
+                    <h2 class="mt-5 pt-5 color-brown">Consulter la liste d'étudiants du Collège Maisonneuve</h2>
                 </p>
-                <a href="" class="btn btn-info btn-lg btn-outline-dark fw-bold mt-3">Afficher la liste</a>
+                <a href="" class="btn btn-success btn-lg fw-bold mt-3">Afficher la liste</a>
             </div>
         </div>
 @endsection
